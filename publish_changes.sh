c60393690335ed41e61f454f3877a16b068f4176
Ryan Gannon <ryanmgannon@gmail.com>
2024-02-13 17:46:51 -0500

    Push changes to deploy

--- publish_changes.sh
+++ publish_changes.sh
@@ -3,6 +3,7 @@
 set -eux
 git commit -am "Push changes to deploy"
 coleslaw deploy
+cp ./favicon.ico deploy/
 git subtree push --prefix deploy origin gh-pages
 git commit -am "New deployment"
 git push
