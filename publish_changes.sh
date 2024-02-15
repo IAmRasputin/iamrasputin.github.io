11bcdb97857de0f5073232a0f585b4bc2c94a6f8
Ryan Gannon <ryanmgannon@gmail.com>
2024-02-14 21:58:57 -0500

    new publish script

--- publish_changes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-set -eux
-git commit -am "Push changes to deploy"
-sbcl --load ./site-deploy.lisp
-git subtree push --prefix deploy origin gh-pages
-git commit -am "New deployment"
-git push
